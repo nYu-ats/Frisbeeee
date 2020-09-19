2ccb78e25af0db75a09ef78dc7acb5078f0b3867
atsushi niu <atsushi.new414@gmail.com>
2020-09-19 14:09:03 +0900

    make frisbee behavior with Translate

---
+++ Assets/Scripts/Disk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Disk : MonoBehaviour
+{
+    public (float x, float y) direction = (30.0f, 30.0f);
+    public float velocity = 50.0f;
+    static float v_x;
+    static float v_y;
+    static float v_z;
+    static float posX = 0.0f;
+    static float posY = 0.0f;
+    static float posZ = 0.0f;
+    static float a_x = -4.9f;
+    static float a_y = -9.8f;
+    float t = 0.0f;
+
+    void Start()
+    {
+        v_x = velocity * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Sin(direction.x * (Mathf.PI / 180.0f));
+        v_y = velocity * Mathf.Sin(direction.y * (Mathf.PI / 180.0f));
+        v_z = velocity * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Cos(direction.x * (Mathf.PI / 180.0f));
+    }
+
+    void Update()
+    {
+        t = Time.deltaTime;
+        v_x += a_x *t;
+        v_y += a_y *t;
+        posX = v_x * t + a_x * Mathf.Pow(t, 2.0f) /2.0f + posX;
+        posY = v_y * t + a_y * Mathf.Pow(t, 2.0f) /2.0f + posY;
+        posZ = v_z * t + posZ;
+        this.transform.Translate(posX, posY, posZ);
+    }
+}
+
---
+++ Assets/Scripts/DiskGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskGenerator : MonoBehaviour
+{
+    static Vector2 pos_start;
+    static Vector2 pos_tmp;
+    static Vector2 pos_end;
+    public GameObject diskPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                //マウス入力取得
+        if(Input.GetMouseButtonDown(0))
+        {
+            pos_start = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            pos_tmp = Input.mousePosition;
+            
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            pos_end = Input.mousePosition;
+            GameObject disk = Instantiate(diskPrefab) as GameObject;
+            disk.GetComponent<Disk>().direction = Orbit.ReleaseDirection(pos_start, pos_end);
+        }
+
+
+    }
+}
+
+public static class Orbit
+{
+    static float maxWidth = 100.0f;
+    static float maxHeight = 100.0f;
+    static float maxRot = 60.0f;
+    static float x = 0.0f;
+    static float y = 0.0f;
+
+    //マウスの移動量から射出方向の回転角度へ変換
+    public static (float, float) ReleaseDirection(Vector2 start, Vector2 end)
+    {
+        Vector2 move = end -start;
+        float move_x = (float)move.x;
+        float move_y = (float)move.y;
+        if((-maxWidth <= move_x) && (move_x <= maxWidth))
+        {
+            x = maxRot * move_x / maxWidth;
+        } 
+        if((-maxHeight <= move_y) && (move_y <= maxHeight))
+        {
+            y = maxRot * move_y / maxHeight;
+        }
+        return (x, y);
+    }
+}
