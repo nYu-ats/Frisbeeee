30a24b768d2fcae262d9bdc106f3e01bec6db2f1
atsushi niu <atsushi.new414@gmail.com>
2020-09-19 14:36:37 +0900

    make frisvee behaviour with AddForce of Rigidbody

--- Assets/Scripts/Disk.cs
+++ Assets/Scripts/Disk.cs
@@ -5,34 +5,36 @@ using System;
 
 public class Disk : MonoBehaviour
 {
-    public (float x, float y) direction = (30.0f, 30.0f);
-    public float velocity = 50.0f;
-    static float v_x;
-    static float v_y;
-    static float v_z;
-    static float posX = 0.0f;
-    static float posY = 0.0f;
-    static float posZ = 0.0f;
-    static float a_x = -4.9f;
-    static float a_y = -9.8f;
-    float t = 0.0f;
+    public (float x, float y) direction = (45.0f, 45.0f);
+    public float addForce;
+    public float decreaseForceX;
+    public float decreaseForceY;
+    static float addForceX;
+    static float addForceY;
+    static float addForceZ;
 
+    public void BehaviourFrisbee(Vector3 dir)
+    {
+        this.GetComponent<Rigidbody>().AddForce(dir);
+    }
     void Start()
     {
-        v_x = velocity * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Sin(direction.x * (Mathf.PI / 180.0f));
-        v_y = velocity * Mathf.Sin(direction.y * (Mathf.PI / 180.0f));
-        v_z = velocity * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Cos(direction.x * (Mathf.PI / 180.0f));
+        addForceX = addForce * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Sin(direction.x * (Mathf.PI / 180.0f));
+        addForceY = addForce * Mathf.Sin(direction.y * (Mathf.PI / 180.0f));
+        addForceZ = addForce * Mathf.Cos(direction.y * (Mathf.PI / 180.0f)) * Mathf.Cos(direction.x * (Mathf.PI / 180.0f));
+        BehaviourFrisbee(new Vector3(addForceX, addForceY, addForceZ));
     }
 
     void Update()
     {
-        t = Time.deltaTime;
-        v_x += a_x *t;
-        v_y += a_y *t;
-        posX = v_x * t + a_x * Mathf.Pow(t, 2.0f) /2.0f + posX;
-        posY = v_y * t + a_y * Mathf.Pow(t, 2.0f) /2.0f + posY;
-        posZ = v_z * t + posZ;
-        this.transform.Translate(posX, posY, posZ);
+        if(direction.x >=0)
+        {
+            BehaviourFrisbee(new Vector3(decreaseForceX, decreaseForceY, 0));
+        }
+        else
+        {
+            BehaviourFrisbee(new Vector3(-decreaseForceX, decreaseForceY, 0));
+        }
     }
 }
 
