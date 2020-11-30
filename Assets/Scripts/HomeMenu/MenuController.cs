d523c7ca7621b06c70c0fb6b540757a74f6b7d38
atsushi niu <atsushi.new414@gmail.com>
2020-12-01 00:18:54 +0900

    add volume and guide switching function

--- Assets/Scripts/HomeMenu/MenuController.cs
+++ Assets/Scripts/HomeMenu/MenuController.cs
@@ -9,6 +9,17 @@ public class MenuController : MonoBehaviour
     [SerializeField] Button[] stageButton;
     [SerializeField] Image[] stageImage;
     [SerializeField] Text[] stageText;
+    [SerializeField] Button[] volumeButton;
+    [SerializeField] Image[] volumeImage;
+    [SerializeField] Text[] volumeText;
+    [SerializeField] Image[] volumeChecked;
+    [SerializeField] Button[] guideButton;
+    [SerializeField] Image[] guideImage;
+    [SerializeField] Text[] guideText;
+    [SerializeField] Image[] guideChecked;
+    private int onFlag = 1;
+    private int offFlag = 0;
+
     private static int startStage;
 
     // Start is called before the first frame update
@@ -23,11 +34,18 @@ public class MenuController : MonoBehaviour
         {
             PlayerPrefs.SetInt("Guide", 1);
         }
+
+        if(!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetInt("Volume", 1);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(PlayerPrefs.GetInt("Volume"));
     }
 
     public void OnStartButtonClicked()
@@ -48,22 +66,59 @@ public class MenuController : MonoBehaviour
         }
     }
 
-    public void OnReturnButtonClicked()
+    public void OnReturnButtonClicked(int flag)
     {
-        foreach(Button obj in stageButton)
+        if(flag == 1)
         {
-            obj.enabled = false;
-        }
+            foreach(Button obj in stageButton)
+            {
+                obj.enabled = false;
+            }
         
-        foreach(Image obj in stageImage)
-        {
-            obj.enabled = false;
-        }
+            foreach(Image obj in stageImage)
+            {
+                obj.enabled = false;
+            }
 
-        foreach(Text obj in stageText)
+            foreach(Text obj in stageText)
+            {
+                obj.enabled = false;
+            }    
+        }
+        else if(flag == 2)
         {
-            obj.enabled = false;
-        }    
+            foreach(Button obj in volumeButton)
+            {
+                obj.enabled = false;
+            }
+        
+            foreach(Image obj in volumeImage)
+            {
+                obj.enabled = false;
+            }
+
+            foreach(Text obj in volumeText)
+            {
+                obj.enabled = false;
+            }
+
+            foreach(Button obj in guideButton)
+            {
+                obj.enabled = false;
+            }
+        
+            foreach(Image obj in guideImage)
+            {
+                obj.enabled = false;
+            }
+
+            foreach(Text obj in guideText)
+            {
+                obj.enabled = false;
+            }
+
+            DisplayOptionSettings(offFlag);  
+        }
     }
 
     public void OnStageButtonclicked(int stageNumber)
@@ -79,5 +134,74 @@ public class MenuController : MonoBehaviour
         gameManager.loadStage = startStage;
         startStage = 1;
         SceneManager.sceneLoaded -= StartPositionSet;
+    }
+
+    public void OnOptionBuutnClicked()
+    {
+        foreach(Button obj in volumeButton)
+        {
+            obj.enabled = true;
+        }
+        
+        foreach(Image obj in volumeImage)
+        {
+            obj.enabled = true;
+        }
+
+        foreach(Text obj in volumeText)
+        {
+            obj.enabled = true;
+        }
+
+        foreach(Button obj in guideButton)
+        {
+            obj.enabled = true;
+        }
+        
+        foreach(Image obj in guideImage)
+        {
+            obj.enabled = true;
+        }
+
+        foreach(Text obj in guideText)
+        {
+            obj.enabled = true;
+        }
+
+        DisplayOptionSettings(onFlag);
+    }
+
+    public void SetVolume(int volume)
+    {
+        PlayerPrefs.SetInt("Volume", volume);
+        foreach(Image obj in volumeChecked)
+        {
+            obj.enabled = false;
+        }
+        volumeChecked[PlayerPrefs.GetInt("Volume")].enabled = true;
+    }
+
+    public void SetGuide(int onOffFlag)
+    {
+        PlayerPrefs.SetInt("Guide", onOffFlag);
+        foreach(Image obj in guideChecked)
+        {
+            obj.enabled = false;
+        }
+        guideChecked[PlayerPrefs.GetInt("Guide")].enabled = true;
+    }
+
+    public void DisplayOptionSettings(int flag)
+    {
+        if(flag == 1)
+        {
+            volumeChecked[PlayerPrefs.GetInt("Volume")].enabled = true;
+            guideChecked[PlayerPrefs.GetInt("Guide")].enabled = true;
+        }
+        else if(flag == 0)
+        {
+            volumeChecked[PlayerPrefs.GetInt("Volume")].enabled = false;
+            guideChecked[PlayerPrefs.GetInt("Guide")].enabled = false;           
+        }
     }
 }
