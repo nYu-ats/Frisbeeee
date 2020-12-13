e6938468d38d853b353f4a7489a9a5875f288023
atsushi niu <atsushi.new414@gmail.com>
2020-12-13 23:37:47 +0900

    modify poll script

--- Assets/Scripts/Polls.cs
+++ Assets/Scripts/Polls.cs
@@ -3,71 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using BLINDED_AM_ME;
 
+/*
+ポールの挙動を制御するスクリプト
+*/
+
 public class Polls : MonoBehaviour
 {
+    //自身のgemeobjectを格納する変数
     private GameObject victim;
+    //ポールの長辺方向の法線を格納する変数
     private Vector3 normalDirection;
+    //衝突が発生した座標を格納する変数
     private Vector3 anchorpoint;
+    //カット前のマテリアル
     private Material capMaterial;
     private Vector3 normalDirectionStd = new Vector3(0.0f, 1.0f, 0.0f);
+    //カット後のマテリアル
     [SerializeField] Material cuttedMaterial;
-    public bool isLive = true;
-    public float globalPositionZ;
+    //rootオブジェクトのZ座標
+    public float rootPositionZ;
+    //ポールが破壊されたときの、ディスク数とカラーバーの値の変化のポップアップ
     [SerializeField] GameObject diskIncreasePopUp;
     [SerializeField] GameObject blueIncreasePopUp;
     [SerializeField] GameObject redIncreasePopUp;
+    //ポップアップの親となるCanvas
     private GameObject uiCanvas;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         victim = this.gameObject;
         capMaterial = victim.GetComponent<Renderer>().material;
-        //切断後のオブジェクトに力を加えられるかのテスト
+        rootPositionZ = victim.transform.root.position.z;
+        //Canvasを設定
+        uiCanvas = GameObject.Find("Canvas");
+        /*
+        切断後のポールに対する処理
+        切断後、オブジェクトを少し回転させる
+        */
         if(victim.name == "right side")
         {
-           victim.GetComponent<Rigidbody>().AddTorque(-30, 0, 0, ForceMode.Impulse); 
+           victim.GetComponent<Rigidbody>().AddTorque(-10, 0, 0, ForceMode.Impulse); 
         }
         else if(victim.name == "left side")
         {
-           victim.GetComponent<Rigidbody>().AddTorque(30, 0, 0, ForceMode.Impulse); 
+           victim.GetComponent<Rigidbody>().AddTorque(10, 0, 0, ForceMode.Impulse); 
         }
-        globalPositionZ = this.gameObject.transform.root.position.z;
-        uiCanvas = GameObject.Find("Canvas");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerExit(Collider collision)
     {
         if(collision.gameObject.tag == "Disk")
         {
+            //Diskが衝突した座標を取得
             anchorpoint = collision.ClosestPointOnBounds(this.transform.position);
-            normalDirection = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z) * normalDirectionStd;
+            //ポールの回転に応じて法線方向を調整
+            normalDirection = Quaternion.Euler(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y, this.transform.rotation.eulerAngles.z) * normalDirectionStd;
+            //colliderを無効にする
             GetComponent<CapsuleCollider>().enabled = false;
-            BLINDED_AM_ME.MeshCut.Cut(victim, anchorpoint, normalDirection , capMaterial, cuttedMaterial, globalPositionZ);
+            //切断処理
+            BLINDED_AM_ME.MeshCut.Cut(victim, anchorpoint, normalDirection , capMaterial, cuttedMaterial, rootPositionZ);
+            //元のポールは消去
             Destroy(victim);
+            //タグでポップアップの種類を判断
             if(this.gameObject.tag == "WhitePoll")
             {
-                GameObject popUp = Instantiate(diskIncreasePopUp);
-                popUp.transform.SetParent(uiCanvas.transform, false);
+                DisplayPopUp(diskIncreasePopUp);
             }
             else if(this.gameObject.tag == "RedPoll")
             {
-                GameObject popUp = Instantiate(redIncreasePopUp);
-                popUp.transform.SetParent(uiCanvas.transform, false);
+                DisplayPopUp(redIncreasePopUp);
             }
             else if(this.gameObject.tag == "BluePoll")
             {
-                GameObject popUp = Instantiate(blueIncreasePopUp);
-                popUp.transform.SetParent(uiCanvas.transform, false);
+                DisplayPopUp(blueIncreasePopUp);
             }
         }
+    }
+
+    private void DisplayPopUp(GameObject obj)
+    {
+        GameObject popUp = Instantiate(obj);
+        //canvasを親オブジェクトに設定
+        popUp.transform.SetParent(uiCanvas.transform, false);
     }
 
 
